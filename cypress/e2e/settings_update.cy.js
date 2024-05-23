ddf0d1f9620432ccaeb66bfc49d7b6fb9c0644a3
Damian Czarnacki <dczarnacki@gmail.com>
2024-05-23 12:51:46 +0200

    Updating new test code

--- cypress/e2e/settings_update.cy.js
+++ cypress/e2e/settings_update.cy.js
@@ -2,59 +2,65 @@
 
 describe('Updating', () => {
     const bio = 'Short bio';
-    const username = 'tester100';
-    const loginMail = 'tester100@wp.pl'
-    const loginPassword = 'tester100'
+    const username = 'tester89';
+    const loginMail = 'tester89@wp.pl'
+    const loginPassword = 'tester89'
 
     let updated;
     
   
     beforeEach(() => {
       cy.visit('user/login');
-      cy.get('[placeholder="Email"]').type(loginMail);
+      cy.get('[placeholder="Email"]').type(loginMail);  
       cy.get('[placeholder="Password"]').type(loginPassword);
-      cy.contains('.btn', 'Sign in').click
+      cy.contains('.btn', 'Sign in').click();
 
       cy.task('generateUpdateData').then((generateUpdateData) => {
         updated = generateUpdateData;
       });
-      
     });
+    
   
     it('should updating bio', () => {
-      cy.visit('/settings');
-      cy.get('[placeholder="Short bio about you"]').click({force: true}).type(updated.newBio);
-      
-      cy.get('.btn').click;
+      cy.get('[href="/settings"]').click();
+      cy.url().should('include', 'settings')
 
+      cy.get('[placeholder="Short bio about you"]').clear().type(updated.newBio);
+      cy.contains('[type="submit"]', 'Update Settings').click();
+      
+      
       cy.visit(`/profile/${username}`);
-      cy.get('.col-xs-12 > p').contains('sport');
+      cy.get('.col-xs-12 > p').contains(updated.newBio);
       
     });
 
     it('should updating username', () => {
-      cy.visit('/settings');
-      cy.get('[placeholder="Username"]').focus().type(updated.newUsername);
-      cy.get('.btn').click;
+      cy.get('[href="/settings"]').click();
+      cy.get('[placeholder="Username"]').clear().type(updated.newUsername);
+      cy.contains('[type="submit"]', 'Update Settings').click();
 
-      cy.visit(`/profile/${username}`);
-      cy.get('h4').should('contain', username);
+      cy.visit(`/profile/${(updated.newUsername).toLowerCase()}`);
+      cy.get('h4').should('contain', (updated.newUsername).toLowerCase());
       
     });
 
     it('should updating email', () => {
-      cy.visit('/settings');
-      cy.get('[placeholder="Email"]')
-        .focus()
-        .type(updated.newEmail);
-      cy.get('.btn').click;
+      cy.get('[href="/settings"]').click();
+      cy.get('[placeholder="Email"]').clear().type(updated.newEmail);
+      cy.contains('[type="submit"]', 'Update Settings').click();
+
+      
+      cy.get('[placeholder="Email"]').should('exist', `value="${updated.newEmail}"`)
+
+      
       
     });
 
     it('should updating password', () => {
-      cy.visit('/settings');
+      cy.get('[href="/settings"]').click();
       cy.get('[placeholder="New Password"]').focus().type(updated.newPassword);
-      cy.get('.btn').click;
+      cy.contains('[type="submit"]', 'Update Settings').click();
     });
+  
 
 });
