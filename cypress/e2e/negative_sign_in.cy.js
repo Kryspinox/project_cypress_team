f5a02d09952a481cf6bdf8ac2e1a8d0d19d51ec5
Ihnat-Zinkevich <ignatzinkevch@gmail.com>
2024-05-24 02:36:31 +0200

    negative_sign_in code corrections

--- cypress/e2e/negative_sign_in.cy.js
+++ cypress/e2e/negative_sign_in.cy.js
@@ -5,71 +5,57 @@ describe('Negative sign in', () => {
 
   let user; 
 
-    beforeEach(() => {
-      cy.visit('/login');
-      beforeEach(() => {
-        cy.task('generateUser').then((generateUser) => {
-
-          user = generateUser;
-
-        });
+  beforeEach(() => {
+    cy.visit('/login');
+    cy.task('generateUser').then((generatedUser) => {
+      user = generatedUser;
     });
-
-it('should not log in with non registered email', () => {
-  cy.registerNewUser.then(user =>{
-
-    cy.findByPlaceholer('Email').type(user.nonRegisteredEmail);
-    cy.findByPlaceholer('Password').type(user.password);
-    cy.submitButton("Sign in");
-
-    cy.contains("email or password: is invalid").should("be visible");
-
   });
-});
+
+  it('should not log in with non-registered email', () => {
+    cy.registerNewUser().then(() => {
+      cy.findByPlaceholder('Email').type(user.randomEmail);
+      cy.findByPlaceholder('Password').type(user.password);
+      cy.get('button[type="submit"]').click();
+
+      cy.contains("email or password is invalid").should("be.visible");
+    });
+  });
 
   it('should not log in with wrong password', () => {
-    cy.registerNewUser.then(user =>{
- 
-    cy.findByPlaceholer('Email').type(user.randomEmail);
-    cy.findByPlaceholer('Password').type(user.wrongPassword);
-    cy.submitButton("Sign in");   
+    cy.registerNewUser().then((registeredUser) => {
+      cy.findByPlaceholder('Email').type(registeredUser.randomEmail);
+      cy.findByPlaceholder('Password').type('wrongPassword');
+      cy.get('button[type="submit"]').click();
 
-    cy.contains("email or password: is invalid").should("be visible");
-
+      cy.contains("email or password is invalid").should("be.visible");
+    });
   });
-});
 
   it('should not log in with empty email field', () => {
-    cy.registerNewUser.then(user =>{
- 
-      cy.findByPlaceholer('Password').type(user.wrongPassword);
-      cy.submitButton("Sign in");
+    cy.registerNewUser().then(() => {
+      cy.findByPlaceholder('Password').type(user.password);
+      cy.get('button[type="submit"]').click();
 
-      cy.contains("email: can't be blank").should("be visible");
+      cy.contains("email can't be blank").should("be.visible");
     });
   });
 
   it('should not log in with empty password field', () => {
-cy.registerNewUser.then(user =>{
- 
-      cy.findByPlaceholer('Email').type(user.randomEmail);
-      cy.submitButton("Sign in");
+    cy.registerNewUser().then(() => {
+      cy.findByPlaceholder('Email').type(user.randomEmail);
+      cy.get('button[type="submit"]').click();
 
-      cy.contains("password: can't be blank").should("be visible");
+      cy.contains("password can't be blank").should("be.visible");
+    });
   });
-});
 
   it('should not log in with empty fields', () => {
-    cy.registerNewUser.then(user =>{
- 
-        cy.findByPlaceholer('Email').type(user.randomEmail);
-        cy.findByPlaceholer('Password').type(user.wrongPassword);
-        cy.submitButton("Sign in");
-  
-        cy.contains("email: can't be blank").should("be visible");
- 
-  });  
-});
+    cy.registerNewUser().then(() => {
+      cy.get('button[type="submit"]').click();
 
-});
+      cy.contains("email can't be blank").should("be.visible");
+    });
+  });
+
 });
