9c56679fdaa1eeddf74f0e65660bbc515133c36e
Ihnat-Zinkevich <ignatzinkevch@gmail.com>
2024-05-24 02:30:02 +0200

    assertions + button negative_sign_in

--- cypress/e2e/negative_sign_in.cy.js
+++ cypress/e2e/negative_sign_in.cy.js
@@ -11,28 +11,65 @@ describe('Negative sign in', () => {
         cy.task('generateUser').then((generateUser) => {
 
           user = generateUser;
-          
+
         });
     });
 
 it('should not log in with non registered email', () => {
+  cy.registerNewUser.then(user =>{
+
+    cy.findByPlaceholer('Email').type(user.nonRegisteredEmail);
+    cy.findByPlaceholer('Password').type(user.password);
+    cy.submitButton("Sign in");
+
+    cy.contains("email or password: is invalid").should("be visible");
 
   });
+});
 
   it('should not log in with wrong password', () => {
+    cy.registerNewUser.then(user =>{
  
+    cy.findByPlaceholer('Email').type(user.randomEmail);
+    cy.findByPlaceholer('Password').type(user.wrongPassword);
+    cy.submitButton("Sign in");   
+
+    cy.contains("email or password: is invalid").should("be visible");
+
   });
+});
 
   it('should not log in with empty email field', () => {
+    cy.registerNewUser.then(user =>{
  
+      cy.findByPlaceholer('Password').type(user.wrongPassword);
+      cy.submitButton("Sign in");
+
+      cy.contains("email: can't be blank").should("be visible");
+    });
   });
 
   it('should not log in with empty password field', () => {
+cy.registerNewUser.then(user =>{
+ 
+      cy.findByPlaceholer('Email').type(user.randomEmail);
+      cy.submitButton("Sign in");
 
+      cy.contains("password: can't be blank").should("be visible");
   });
+});
 
-  it('should log in with empty fields', () => {
+  it('should not log in with empty fields', () => {
+    cy.registerNewUser.then(user =>{
+ 
+        cy.findByPlaceholer('Email').type(user.randomEmail);
+        cy.findByPlaceholer('Password').type(user.wrongPassword);
+        cy.submitButton("Sign in");
+  
+        cy.contains("email: can't be blank").should("be visible");
  
   });  
+});
+
 });
 });
